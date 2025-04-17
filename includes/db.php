dfda53f14ad8014a8b610d92ed1979121c36f079
Jaspreet Singh <34242488+jaspreet97977@users.noreply.github.com>
2025-04-16 23:37:06 -0400

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,13 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
-$client = new MongoDB\Client("var");
+// Directly add your MongoDB URI here (replace with your actual connection string)
+$client = new MongoDB\Client("mongodb+srv://ramandeepkourconestoga:rdcxhuFqepY9ssxa@cluster0.k9dth.mongodb.net/NextGenTechDb");
+
+// Access the database
 $db = $client->NextGenTech;
+
+// Access the collections
 $collection = $db->products;
 $reviewsCollection = $db->reviews;
 $usersCollection = $db->users;
